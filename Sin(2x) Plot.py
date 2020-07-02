62914c85da24d74bcd712293935b25d63fe600ba
ryanm2427 <67718230+ryanm2427@users.noreply.github.com>
2020-07-01 21:22:42 -0700

    Add files via upload

---
+++ Sin(2x) Plot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-1*np.pi,1*np.pi,0.01)
+y = np.sin(2*x)
+plt.plot(x,y)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
